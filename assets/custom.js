9c7f1a5de59d2663fe9d1798993fee7582ff19ae
munir <munir@anodyne.dk>
2022-12-08 13:52:05 +0100

    productpage counter added

--- assets/custom.js
+++ assets/custom.js
@@ -74,11 +74,51 @@
   }
 
 
-  // Every day counter 
+  const counter = document.getElementById("product__counter");
+  if(counter){
+  setInterval(() => {
+	  //set day hours
+	  const d = new Date();
+	  const setTimeOfDay = d.getHours();
+	  //set day hours
+	  let	date1 = new Date().setHours(08,59,59) 
+	  let date2 =  new Date().setHours(14,59,59) 
+	  let date3 = new Date().setHours(32,59,59) 
+  
+	const currentDate = d;
+	const timeBetweenDates1 = Math.ceil(( date1 - currentDate ) / 1000)
+	  const timeBetweenDates2 = Math.ceil(( date2 - currentDate ) / 1000);
+	  const timeBetweenDates3 = Math.ceil(( date3 - currentDate ) / 1000)
+			  if(setTimeOfDay >= 0 && setTimeOfDay < 9){
+				  if(timeBetweenDates1 < 0) return
+				  countTimer(timeBetweenDates1)
+			  }
+			  else if(setTimeOfDay >= 9 && setTimeOfDay < 15){
+				  if(timeBetweenDates2 < 0) return
+				  countTimer(timeBetweenDates2)
+			  }
+			  else{
+				  if(timeBetweenDates3 < 0) return
+				  countTimer(timeBetweenDates3)
+			  }
+	  }, 1000)
+  //add zeros
+  function addZero(num) {
+	  return ("0" + parseInt(num)).substr(-2);
+  }
+  function countTimer(time){
+	  const seconds = addZero(time % 60)
+	  const minutes = addZero(Math.floor(time / 60) % 60)
+	  const hours = addZero(Math.floor(time / 3600))
+		counter.innerHTML = "<div class='inner'>"+hours+"<span class='dots'>:</span></div><div class='inner'>"+minutes+"<span class='dots'>:</span></div><div class='inner'>"+seconds+"</div>";
+  }
+  }
+  
+  
   const closeBtn = document.querySelector(".close__btn");
   const flipBar = document.querySelector(".flip-container");
   
-  
+  if(flipBar){
   window.addEventListener("scroll", () => {
 	  let scroll = this.scrollY;
 	  if(scroll > 699){ flipBar.classList.add("fixed-bottom")
@@ -86,15 +126,17 @@
 	  else if(scroll < 500) {flipBar.classList.remove("fixed-bottom")
 	}
 	});
+  if(closeBtn){
+	  closeBtn.addEventListener("click", ()=>{
+		  localStorage.setItem("className", "show-flip");
+		  let classFromLocalStorage = localStorage.getItem("className");
+		  if (!classFromLocalStorage){
+			  flipBar.classList.add("show-flip")
+		  }
+		  flipBar.classList.remove("show-flip");
+	  });
+  }
   
-  closeBtn.addEventListener("click", ()=>{
-	  localStorage.setItem("className", "show-flip");
-	  let classFromLocalStorage = localStorage.getItem("className");
-	  if (!classFromLocalStorage){
-		  flipBar.classList.add("show-flip")
-	  }
-	  flipBar.classList.remove("show-flip");
-  });
   
   const checkCookie = () => {
 	  let classFromLocalStorage = localStorage.getItem("className");
@@ -203,4 +245,4 @@
   
   flipCard.append(topFlip, bottomFlip);
   }
-  
+  }
